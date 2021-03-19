b29c090d9b1d5d4ce01092fda2e91a04e209339e
Your Name <you@example.com>
2021-03-19 18:18:27 +0500

    create branch for plotly.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Plot from 'react-plotly.js';
+import Data from './dep/data.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: Data.data, layout: Data.layout, frames: [], config: {}};
+    }
+
+    render() {
+        return (
+
+            <Plot
+                data={this.state.data}
+                layout={this.state.layout}
+                frames={this.state.frames}
+                config={this.state.config}
+                onInitialized={(figure) => this.setState(figure)}
+                onUpdate={(figure) => this.setState(figure)}
+            />
+        );
+    }
 }
 
 export default App;
